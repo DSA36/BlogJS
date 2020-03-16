8bf95eb033f326ccbd78456108d54e42998a962e
Sergey <sergey@example.com>
2020-03-16 16:49:03 +0300

    commit API fetch 1

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,7 +1,13 @@
-import { Component } from "../core/component";
+import { Component } from '../core/component'
+import {apiService} from '../services/api.service'
 
 export class PostComponent extends Component {
   constructor(id) {
     super(id)
+  }
+
+  async onShow() {
+    const data = await apiService.fetchPosts()
+    console.log('DATA', data)
   }
 }
