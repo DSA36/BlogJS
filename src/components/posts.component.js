38282f2fc7270047bd07d19f2c9dff53b06faf56
Sergey <sergey@example.com>
2020-03-17 14:34:12 +0300

    commit transform fb post to Array +id

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/component'
 import {apiService} from '../services/api.service'
+import {TransformService} from '../services/transform.service'
 
 export class PostComponent extends Component {
   constructor(id) {
@@ -7,7 +8,8 @@ export class PostComponent extends Component {
   }
 
   async onShow() {
-    const data = await apiService.fetchPosts()
-    console.log('DATA', data)
+    const fbData = await apiService.fetchPosts()
+    const posts = TransformService.fbObjectToArray(fbData)
+    console.log(posts)
   }
 }
