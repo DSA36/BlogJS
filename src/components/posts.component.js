6c78ec043fe98965b96db22c442e20b9f2603dfb
Sergey <sergey@example.com>
2020-03-18 16:46:44 +0300

    commit rendering post from fb

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -10,6 +10,34 @@ export class PostComponent extends Component {
   async onShow() {
     const fbData = await apiService.fetchPosts()
     const posts = TransformService.fbObjectToArray(fbData)
-    console.log(posts)
+    const html = posts.map(post => renderPost(post))
+
+    this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
   }
+
+  onHide() {
+    this.$el.innerHTML = ''
+  }
+}
+
+function renderPost(post) {
+  const button = '<button class ="button-round button-small button-primary">Сохранить</button>'
+  
+  return `
+    <div class="panel">
+          <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags hide">
+              <li class="tag tag-blue tag-rounded">Новость</li>
+            </ul>
+          </div>
+          <div class="panel-body">
+            <p class="multi-line">${post.fulltext}</p>
+          </div>
+          <div class="panel-footer w-panel-footer">
+            <small>${post.date}</small>
+            ${button}
+          </div>
+        </div>
+  `
 }
